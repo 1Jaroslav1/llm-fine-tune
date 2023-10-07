4e23435b500abb577a08f594ebc79e657dc0ccb6
1Jaroslav1 <garbar.jarek@gmail.com>
2023-10-07 16:26:39 +0000

    feat: add useful script to run new vm

---
+++ start.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get install python3-venv
+python3 -m venv myenv
+source myenv/bin/activate
+pip install jupyter ipykernel
+python -m ipykernel install --user --name=myenv
+
+# ssh-keygen
